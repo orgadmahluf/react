e6b3207dc23d66a82fe76b08cef5092608ff3641
orgad <orgad@svcollege.co.il>
2019-12-30 20:52:09 +0200

    Updates

---
+++ precache-manifest.cff39603ac90fcb63d66d1bba1439ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61667be06098ee282ed91c6fe8671c5b",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "fcb3385ddd6c637f22fa",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bcb1e42d89fed9530581",
+    "url": "/react/static/js/2.3ae8199d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react/static/js/2.3ae8199d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcb3385ddd6c637f22fa",
+    "url": "/react/static/js/main.e8d3a90d.chunk.js"
+  },
+  {
+    "revision": "e6d4e74a2d184da8fca6",
+    "url": "/react/static/js/runtime-main.0d876c3f.js"
+  }
+]);
